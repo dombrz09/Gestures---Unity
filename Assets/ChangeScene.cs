e01adddccde14e47086f94fa276f1c4c80c039db
JaruzeI <grzemix@vp.pl>
2017-11-13 11:02:41 +0100

    Fixed dropdown buttons to hide it's content when pointer exits their area.

--- Assets/ChangeScene.cs
+++ Assets/ChangeScene.cs
@@ -1,4 +1,16 @@
-﻿using System.Collections;
+﻿/////////////////////////////////////////////////
+//                  CREATE                      
+//Author:       Grzegorz Jaruszewski
+//Date:         2017-11-12
+//Description:  Obracanie obiektu
+/////////////////////////////////////////////////
+//                  CHANGE                      
+//Author:
+//Date:
+//Description:
+/////////////////////////////////////////////////
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
