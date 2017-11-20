03e04f8f3124495313adf9c98ba88a01af6a252d
JaruzeI <grzemix@vp.pl>
2017-11-20 13:35:56 +0100

    Changed camera movement and changed buttons / panels apperance

--- Assets/RotateTarget.cs
+++ Assets/RotateTarget.cs
@@ -5,6 +5,11 @@
 //Description:  Obracanie obiektu
 /////////////////////////////////////////////////
 //                  CHANGE                      
+//Author:       Grzegorz Jaruszewski
+//Date:         2017-11-20
+//Description:  Zmiana sposobu działania kamery
+/////////////////////////////////////////////////
+//                  CHANGE                      
 //Author:
 //Date:
 //Description:
@@ -20,29 +25,92 @@ public class RotateTarget : MonoBehaviour
 
 
     //Speed
-    private float speed = 50f;
+    private float speed = 10f;
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
 
     void Start() { }
 
     void Update()
     {
 
+        //Sprawdzenie czy lewy przycisk myszy jest wciśniety
+        if (((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) || Input.GetMouseButton(0)) && target != null)
+            moveCameraForward();
+
         //Sprawdzenie czy prawy przycisk myszy jest wciśniety
         if (((Input.touchCount > 0 && Input.GetTouch(1).phase == TouchPhase.Began) || Input.GetMouseButton(1)) && target != null)
-            moveCamera();
+            moveCameraBack();
+
+        //Sprawdzenie czy środkowy przycisk myszy jest wciśniety
+        if (((Input.touchCount > 0 && Input.GetTouch(2).phase == TouchPhase.Began) || Input.GetMouseButton(2)) && target != null)
+            rotateCamera();
     }
 
-
-    //procedura sprawdzająca, która opcja obrotu ma zostać wykonana
-    void moveCamera()
+    void rotateCamera()
     {
+        /*
         if (Input.GetAxis("Mouse X") > 0f) //kierunek : prawo
-            transform.RotateAround(target.transform.position, Vector3.up, Time.deltaTime * speed);
-        if (Input.GetAxis("Mouse X") < 0f) //kierunek : lewo
             transform.RotateAround(target.transform.position, Vector3.down, Time.deltaTime * speed);
+        if (Input.GetAxis("Mouse X") < 0f) //kierunek : lewo
+            transform.RotateAround(target.transform.position, Vector3.up, Time.deltaTime * speed);
         if (Input.GetAxis("Mouse Y") > 0f) //kierunek : góra
-            transform.RotateAround(target.transform.position, Vector3.left, Time.deltaTime * speed);
-        if (Input.GetAxis("Mouse Y") < 0f) //kierunek : dół
             transform.RotateAround(target.transform.position, Vector3.right, Time.deltaTime * speed);
+        if (Input.GetAxis("Mouse Y") < 0f) //kierunek : dół
+            transform.RotateAround(target.transform.position, Vector3.left, Time.deltaTime * speed);
+        */
+
+            yaw += speedH * Input.GetAxis("Mouse X");
+            pitch -= speedV * Input.GetAxis("Mouse Y");
+
+            transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
     }
+
+    void moveCameraForward()
+    {
+        transform.Translate(Vector3.forward / 10);
+    }
+
+    void moveCameraBack()
+    {
+        transform.Translate(Vector3.back / 10);
+    }
+
+    /*
+    //poruszanie kamera po osiach XY
+    void moveCameraXY()
+    {
+        if (Input.GetAxis("Mouse Y") > 0)
+        {
+            transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed, 0.0f);
+        }
+
+        else if (Input.GetAxis("Mouse Y") < 0)
+        {
+            transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed, 0.0f);
+        }
+    }
+
+    //poruszanie kamera po osiach XZ
+    void moveCameraXZ()
+    {
+        if (Input.GetAxis("Mouse X") > 0)
+        {
+            transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed,
+                                       0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed);
+        }
+
+        else if (Input.GetAxis("Mouse X") < 0)
+        {
+            transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed,
+                                       0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed);
+        }
+    }
+    */
+    //procedura sprawdzająca, która opcja obrotu ma zostać wykonana
 }
