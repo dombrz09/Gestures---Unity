af41c2d64fd64c0e1048c89e2180bd0964f9e821
dawidsklorz <dawidsklorz@gmail.com>
2017-11-21 23:56:58 +0100

    add presentation system

---
+++ Assets/Load3dObject.cs
@@ -0,0 +1,35 @@
+﻿/////////////////////////////////////////////////
+//                  CREATE                      
+//Author:       Dawid SKlorz
+//Date:         2017-11-18
+//Description:  Pobranie ścieżki do obiektu
+/////////////////////////////////////////////////
+//                  CHANGE                      
+//Author:       
+//Date:         
+//Description:  
+/////////////////////////////////////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEditor;
+using System.Text;
+using UnityEngine.UI;
+
+public class Load3dObject : MonoBehaviour {
+
+    public Text objectPath;
+
+    public void load3dObject()
+    {
+        //ścieżka do pliku .obj
+        //string path = EditorUtility.OpenFilePanel("Load 3D Object", "", "obj");
+        string path = System.IO.Directory.GetCurrentDirectory() + "\\Assets\\tinker.obj";
+        if (path.Length > 0)
+        {
+            objectPath.text = path;
+        }
+    }
+
+}
--- Assets/MeshObject.cs
+++ Assets/MeshObject.cs
@@ -5,35 +5,43 @@
 //Description:  Ladowanie obiektu 3D
 /////////////////////////////////////////////////
 //                  CHANGE                      
-//Author:
-//Date:
-//Description:
+//Author:       Dawid Sklorz
+//Date:         2017-11-18
+//Description:  Wczytanie obiektu po podaniu jego ścieżki
 /////////////////////////////////////////////////
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MeshObject : MonoBehaviour {
 
-	void Start () {}
-	
-	void Update () {
-        //objekt do importowanie plikow w formacie .obj
-        var objImporter = new ObjImporter();
+    public Text objectPath;
 
-        //właściwe załadowanie pliku do pamięci
-        Mesh loadedMesh = objImporter.ImportFile("C:\\tinker.obj");
+    void Start () {}
 
-        //pobranie referencji do objektu w unity
-        MeshFilter unityMesh = GetComponent<MeshFilter>();
-        Mesh myMesh = unityMesh.mesh;
+    void Update()
+    {
+        //sprawdzenie czy podano ścieżkę do obiektu
+        if (this.objectPath.text.Length > 0)
+        {
+            //objekt do importowanie plikow w formacie .obj
+            var objImporter = new ObjImporter();
 
-        //przekopiowanie objektu pobranego z pliku do unity
-        myMesh.Clear();
-        myMesh.vertices = loadedMesh.vertices;
-        myMesh.triangles = loadedMesh.triangles;
-        myMesh.uv = loadedMesh.uv;
-        myMesh.RecalculateNormals();
+            //właściwe załadowanie pliku do pamięci
+            Mesh loadedMesh = objImporter.ImportFile(this.objectPath.text);
+
+            //pobranie referencji do objektu w unity
+            MeshFilter unityMesh = GetComponent<MeshFilter>();
+            Mesh myMesh = unityMesh.mesh;
+
+            //przekopiowanie objektu pobranego z pliku do unity
+            myMesh.Clear();
+            myMesh.vertices = loadedMesh.vertices;
+            myMesh.triangles = loadedMesh.triangles;
+            myMesh.uv = loadedMesh.uv;
+            myMesh.RecalculateNormals();
+        }
     }
 }
