601197789c64de1c0ab85f0fcc87af4dbe787a8f
Dominika <dombrz2@st.amu.edu.pl>
2017-12-04 00:27:27 +0100

    clear object and clear all

---
+++ Assets/ClearObject.cs
@@ -0,0 +1,42 @@
+﻿/////////////////////////////////////////////////
+//                  CREATE                      
+//Author:       Dominika Brzozowska
+//Date:         2017-12-03
+//Description:  Czyszcenie obiektów
+/////////////////////////////////////////////////
+//                  CHANGE                      
+//Author:       
+//Date:         
+//Description:  
+/////////////////////////////////////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClearObject : MonoBehaviour {
+
+    private bool check_status;
+
+    void OnMouseDrag()
+    {
+        if (check_status == true)
+        Destroy(gameObject);
+    }
+
+   public void clear_object()
+    {
+        check_status = true;
+    }
+
+    public void stop_clearing()
+    {
+        check_status = false;
+    }
+
+   public void reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
